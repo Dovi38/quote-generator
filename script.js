c8ec254d5562ac11ec14f69d4b9a4139ac61275e
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2024-03-18 20:38:45 +0000

    Js file complete

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+const quoteContainer = document.querySelector("#quote-container");
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
 
+let apiQuotes = [];
+
+//New quote function
+const newQuote = () => {
+  //pic random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //check quote length to determine style
+  if (quote.text.length > 40) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+};
+
+//Get quotes from API
+const getQuotes = async () => {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //handle error alert(error)
+  }
+};
+//Tweet a quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+};
+//Event listeners. first declare function then add event listener
+
+newQuoteBtn.addEventListener("click", newQuote);
+
+twitterBtn.addEventListener("click", tweetQuote);
+
+//On load
+getQuotes();
